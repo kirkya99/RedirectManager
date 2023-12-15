7c519d3e6ed5238905076c306c97048f6c68c423
kirkya99 <fabianschotte@outlook.com>
2023-12-15 16:38:23 +0100

    functions are working

--- index.js
+++ index.js
@@ -1,68 +1,19 @@
-// const express = require("express");
-// const fs = require("fs");
-// const app = express();
-// const filename = __dirname + "/storage.json";
-// const port = 3000
-// const token = process.env.BEARER_TOKEN
-
-// //Middleware
-// app.use(express.json()); //for parsing application/json
-// // app.use(cors()); //for configuring Cross-Origin Resource Sharing (CORS)
-// function log(req, res, next) {
-//     console.log(req.method + " Request at" + req.url);
-//     next();
-// }
-// app.use(log);
-
-// // Dieser Endpunkt soll dazu dienen, dass die slug zur entsprechenden
-// // Domain aufgelöst und mittels express.redirect() umgeleitet wird
-// app.get('/:slug', (req, res) => {
-    
-// });
-
-// // Soll alle Einträge aus der JSON‐Datei ausgeben
-// app.get('/entries', (req, res) => {
-//     const entries = loadDataFromFile();
-//     res.json(entries); 
-// });
-
-// // Eintrag mit der gegebenen Slug aus der Datei entfernen
-// app.delete('/entry/:slug', (req, res) => {
-//     const { slug } = req.params;
-//     let entries = loadDataFromFile();
-//     entries = entries.filter(entry => entry.slug !== slug);
-//     saveDataToFile(entries);
-//     res.json({ message: 'Entry deleted successfully' });
-// });
-
-// // Soll eine URL und eine Slug entgegennehmen zum Abspeichern für
-// // späteres weiterleiten. Wird keine Slug mitgegeben, soll eine zufällige
-// // generiert werden.
-// app.post('/entry', (req, res) => {
-
-// });
-
-// // Funktion zum Lesen der storage.json
-// const readStorage = () => {
-//     return JSON.parse(fs.readFileSync('storage.json', 'utf8'));
-// };
-
-// // Funktion zum Speichern von Daten in die JSON-Datei
-// function saveDataToFile(data) {
-//     fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf-8');
-// }
-
 const express = require('express');
 const fs = require('fs');
 const app = express();
 app.use(express.json());
-const token = process.env.BEARER_TOKEN || 'StandardToken';
+const token = 123456;
 
 // Middleware für Authentifizierung
 const authenticate = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || authHeader !== `Bearer ${token}`) {
-        return res.status(401).send('Unauthorized');
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        if (bearerToken !== token) {
+            return res.status(401).send('Unauthorized');
+        }
     }
     next();
 };
@@ -71,6 +22,11 @@ const authenticate = (req, res, next) => {
 const readStorage = () => {
     return JSON.parse(fs.readFileSync('storage.json', 'utf8'));
 };
+
+// Route zum Anzeigen aller Einträge
+app.get('/entries', authenticate, (req, res) => {
+    res.json(readStorage());
+});
 
 // Route zum Umleiten basierend auf dem Slug
 app.get('/:slug', (req, res) => {
@@ -82,11 +38,6 @@ app.get('/:slug', (req, res) => {
     } else {
         res.status(404).send('Slug not found');
     }
-});
-
-// Route zum Anzeigen aller Einträge
-app.get('/entries', authenticate, (req, res) => {
-    res.json(readStorage());
 });
 
 // Route zum Hinzufügen eines neuen Eintrags
